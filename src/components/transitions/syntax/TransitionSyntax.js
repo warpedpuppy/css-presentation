e6d88740f40fe7f6407b6b5ad8368ea6af9ad796
ted <ted@warpedpuppy.com>
2023-03-20 09:39:00 -0400

    added list template to transition syntax page

--- src/components/transitions/syntax/TransitionSyntax.js
+++ src/components/transitions/syntax/TransitionSyntax.js
@@ -1,54 +1,46 @@
+import ListTemplate from "../../../templates/ListTemplate";
 import "./TransitionSyntax.css";
 function TransitionSyntax() {
   return (
-    <section id="syntax-list">
-      <div>
-        <h4>properties:</h4>
-        <ul>
-          <li>
-            <input type="checkbox" />
-            <div className="label">transition-property</div>
-            <div className="discussion">discussion</div>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <div className="label">transition-duration</div>
-            <div className="discussion">discussion</div>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <div className="label">transition-timing-function</div>
-            <div className="discussion">discussion</div>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <div className="label">transition-delay</div>
-            <div className="discussion">discussion</div>
-          </li>
-        </ul>
+    <section id="transition-syntax">
+      <ListTemplate
+        title="properties"
+        labels={[
+          "transition-property",
+          "transition-duration",
+          "transition-timing-function",
+          "transition-delay",
+        ]}
+        content={[]}
+      />
 
-        <h4>short hand:</h4>
-        <ul>
-          <li>2 property -- name duration</li>
-          <li>3 -- name duration delay|easingfunction</li>
-          <li>4 -- name duration easing delay</li>
-          <li>mutltiple properties: chain with commas</li>
-          <li>all</li>
-        </ul>
+      <ListTemplate
+        title="short hand"
+        labels={[
+          "2 property -- name duration",
+          "3 -- name duration delay|easingfunction",
+          "4 -- name duration easing delay",
+          "mutltiple properties: chain with commas",
+          "all",
+        ]}
+        content={[]}
+      />
 
-        <h4>definitive discussion:</h4>
-        <ul>
-          <li>
-		  <div className="label"><a
+      <ListTemplate
+        title="definitive discussion"
+        labels={[
+          [
+            <a
               href="https://developer.mozilla.org/en-US/docs/Web/CSS/transition"
               target="_blank"
               rel="noreferrer"
             >
               mdn
-            </a></div>
-          </li>
-        </ul>
-      </div>
+            </a>,
+          ],
+        ]}
+        content={[]}
+      />
     </section>
   );
 }
