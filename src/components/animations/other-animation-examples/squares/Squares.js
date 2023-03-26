c4a611b0609d49e0330cb1bc0f5ce964656676b1
ted <ted@warpedpuppy.com>
2023-03-26 13:08:05 -0400

    fixed squares

--- src/components/animations/other-animation-examples/squares/Squares.js
+++ src/components/animations/other-animation-examples/squares/Squares.js
@@ -1,21 +1,25 @@
 import './Squares.css';
 function Squares() {
-	return ( <svg id="squares" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 250">
-		<rect class="leftBack vertBox" />
-		<rect class="rightBack vertBox" />
-		<rect class="topBack horizBox" />
-		<rect class="bottomBack horizBox" />
-		  
-		<rect class="leftMiddle vertBox" />
-		<rect class="rightMiddle vertBox" />
-		<rect class="topMiddle horizBox" />
-		<rect class="bottomMiddle horizBox" />
-		  
-		<rect class="leftTop vertBox" />
-		<rect class="rightTop vertBox" />
-		<rect class="topTop horizBox" />
-		<rect class="bottomTop horizBox" />
-		</svg> );
+	return ( <svg id="squares"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 250">
+
+	<rect class="leftBack vertBox" height="250" width="250"/>
+	<rect class="rightBack vertBox" height="250" width="250"/>
+
+	 <rect class="topBack horizBox" height="250" width="500"/>
+	<rect class="bottomBack horizBox" height="250" width="500"/>
+	  
+	<rect class="leftMiddle vertBox"  height="250" width="250"/>
+	<rect class="rightMiddle vertBox"  height="250" width="250"/>
+
+	 <rect class="topMiddle horizBox" height="250" width="500"/>
+	<rect class="bottomMiddle horizBox" height="250" width="500"/>
+	  
+	<rect class="leftTop vertBox"  height="250" width="250"/>
+	<rect class="rightTop vertBox"  height="250" width="250"/> 
+
+	<rect class="topTop horizBox" height="250" width="500"/>
+	<rect class="bottomTop horizBox" height="250" width="500"/>
+	</svg> );
 }
 
 export default Squares;
