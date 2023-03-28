88834392dc50c35a3f14d819774498a4d0550e4d
ted <ted@warpedpuppy.com>
2023-03-28 12:35:46 -0400

    changed easing graphics

--- src/components/easing/Easing.js
+++ src/components/easing/Easing.js
@@ -2,60 +2,106 @@ import TabTemplate from "../../templates/TabTemplate";
 import "./Easing.css";
 
 function Examples() {
-	return ( <section id="easing">
-	<div id="track-0" className="track">
-	  <h4>ease</h4>
-	  <img src="/images/train.png" alt="train" />
-	</div>
-	<div id="track-1" className="track">
-	  <h4>linear</h4>
-	  <img src="/images/train.png" alt="train" />
-	</div>
-	<div id="track-2" className="track">
-	  <h4>ease-in</h4>
-	  <img src="/images/train.png" alt="train" />
-	</div>
-	<div id="track-3" className="track">
-	  <h4>ease-out</h4>
-	  <img src="/images/train.png" alt="train" />
-	</div>
-	<div id="track-4" className="track">
-	  <h4>ease-in-out</h4>
-	  <img src="/images/train.png" alt="train" />
-	</div>
-	<div id="track-5" className="track">
-	  <h4>custom</h4>
-	  <img src="/images/train.png" alt="train" />
-	  <div class="easing-credit">
-		credit for the custom one:
-		<a
-		  href="https://codepen.io/setswon/pen/VJeXXq"
-		  rel="noreferrer"
-		  target="_blank"
-		>
-		  https://codepen.io/setswon/pen/VJeXXq
-		</a>
-	  </div>
-	</div>
-  </section>)
+  return (
+    <section id="easing">
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>ease</h4>
+      </div>
+
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>linear</h4>
+      </div>
+
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>ease-in</h4>
+      </div>
+
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>ease-out</h4>
+      </div>
+
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>ease-in-out</h4>
+      </div>
+
+      <div className="ease-example">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 50">
+          <rect class="cls-1" width="500" height="50" />
+          <polygon
+            class="arrow"
+            points="16.98 16.78 16.98 5 40 25 16.98 45 16.98 33.22 0 33.22 0 16.78 16.98 16.78"
+          />
+        </svg>
+        <h4>custom</h4>
+        <div class="easing-credit">
+          credit for the custom one:
+          <a
+            href="https://codepen.io/setswon/pen/VJeXXq"
+            rel="noreferrer"
+            target="_blank"
+          >
+            https://codepen.io/setswon/pen/VJeXXq
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function Visualizer() {
-	return (
-		<a
-        className="big-button"
-        href="https://matthewlein.com/tools/ceaser"
-        target="_blank"
-        rel="noreferrer"
-      >
-        easing visualization tool
-      </a>
-	)
+  return (
+    <a
+      className="big-button"
+      href="https://matthewlein.com/tools/ceaser"
+      target="_blank"
+      rel="noreferrer"
+    >
+      easing visualization tool
+    </a>
+  );
 }
 
 function Easing() {
   return (
-   <TabTemplate links={['examples', 'link to visualizer']} content={[<Examples />, <Visualizer />]} />
+    <TabTemplate
+      links={["examples", "link to visualizer"]}
+      content={[<Examples />, <Visualizer />]}
+    />
   );
 }
 
